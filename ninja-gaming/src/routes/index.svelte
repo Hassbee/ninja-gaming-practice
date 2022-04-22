3e7eb7f4f4719122aa2014f11543009ad68fd651
Hassbee <hassbee720@gmail.com>
2022-04-22 03:32:37 +0000

    initial

---
+++ ninja-gaming/src/routes/index.svelte
@@ -0,0 +1,25 @@
+<script>
+  
+</script>
+
+
+
+<div class="index">
+
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium id quod aspernatur.
+Temporibus eum culpa quo nisi eveniet quis illum, 
+expedita sint? Repellendus quam nulla laborum beatae voluptate quia est hic minus expedita blanditiis
+ quisquam dolore corrupti veritatis, architecto aperiam culpa sunt. Numquam, 
+consectetur velit.
+</p>
+
+
+</div>
+
+<style>
+    .index {
+        text-align:center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
